311e845ae5315ab1d941433c86762cf7f21e609e
Tommaso P <tommaso.plinio@gmail.com>
2021-03-18 16:24:34 +0100

    lista spesa clone

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 var listaSpesa = [
-  'comprare acqua'
-  'comprare biscotti'
-  'comprare latte'
-  'comprare mele'
-]
+  'acqua',
+  'biscotti',
+  'latte',
+  'mele',
+];
+
+// ciclo per creare nuovo elementi
+for (var i = 0; i < listaSpesa.length; i++) {
+  // 1 clono il template
+  var template = $('.template li').clone();
+  // 2 aggiungo le info al template
+  template.prepend(listaSpesa[i]);
+  // 3 aggiungo il template alla lista
+  $('.todo-list').append(template);
+}
